1f45577dc98de3099711242ed76d200383c6fa0c
Nguyễn Phúc <ngtranthienphuc29@gmail.com>
2026-04-24 22:49:59 +0700

    V4 update

--- private_credentials.ino
+++ private_credentials.ino
@@ -1,9 +1,9 @@
 void sysFetchCreds() {
-  OPENAI_KEY =         "";
-  GROQ_KEY =           "";   
-  ELEVENLABS_KEY =     "";
-  DEEPGRAM_KEY =       "";
   wmSsid =      "";      // WiFi credentials are optional, declare as default for debugging, if not then configure through WM later on.
   wmPwd =       ""; 
+  
+  AUTHOR_EMAIL = "";
+  AUTHOR_PASSWORD = "";
+  RECIPIENT_EMAIL = "";
 }
 
